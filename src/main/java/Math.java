76680637545a6fa90e03cb8fc0579c723676137c
hhimanshu <harit.himanshu@yahoo.in>
2018-10-26 11:55:33 -0400

    Added subtract

--- src/main/java/Math.java
+++ src/main/java/Math.java
@@ -2,4 +2,7 @@ class Math {
   static int sum(final int a, final int b) {
     return a + b;
   }
+  static int subtract(final int a, final int b) {
+    return a - b;
+  }
 }
--- src/test/java/MathTest.java
+++ src/test/java/MathTest.java
@@ -7,4 +7,9 @@ public class MathTest {
   public void testSum() {
     assertEquals("sum should be equal", 4, Math.sum(1, 3));
   }
+
+  @Test
+  public void testSubtract() {
+    assertEquals("subtract should be equal", 2, Math.sum(4, 2));
+  }
 }
